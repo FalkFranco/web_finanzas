1f464df396f574037dc6b15be707b8a5873e3eca
FalkFranco <francofalk@gmail.com>
2024-09-11 22:40:45 -0300

    funcionando

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,118 @@
 @extends('layouts.app')
 
 @section('content_landing')
+    {{-- <section class="vh-100">
+        <div class="row h-100">
+            <div class="col-lg-6 col-12 bg-black d-flex flex-column align-items-center justify-content-center">
+                <div class="container-md">
+                    <h1 class="fw-bold text-white pb-3">Iniciar Sesión</h1>
+                    <p class="pb-3 mb-3 mb-lg-4 text-white">
+                        ¿No posees cuenta?
+                        <a href="#" class="ms-2 text-primary">
+                            ¡Regístrate aquí!
+                        </a>
+                    </p>
+                    <form action="">
+                        <div class="pb-3 mb-3">
+                            <div class="position-relative">
+                                <i
+                                    class="ri-mail-line fs-lg position-absolute top-50 start-0 translate-middle-y ms-3 text-grey"></i>
+                                <input class="form-control form-control-lg ps-5" type="email" placeholder="Email address"
+                                    required />
+                            </div>
+                        </div>
+                        <div class="pb-3 mb-3">
+                            <div class="position-relative">
+                                <i
+                                    class="ri-lock-line fs-lg position-absolute top-50 start-0 translate-middle-y ms-3 text-grey"></i>
+                                <input class="form-control form-control-lg ps-5" type="password" placeholder="Password"
+                                    required />
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-lg btn-primary w-100 mb-4">
+                            Iniciar Sesión
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-6 d-none d-lg-block bg-secondary d-flex align-items-center justify-content-center p-0">
+                <img src="{{ asset('img/finanza_bg.jpg') }}" alt="Login" class="img-fluid w-100 h-100" />
+            </div>
+        </div>
+    </section> --}}
+    <section class="login">
+        <div class="vh-100">
+            <div class="row h-100">
+                <div class="col-lg-6 col-12 bg-black d-flex flex-column align-items-center justify-content-center">
+                    <div class="container-md container">
+                        <h1 class="fw-bold text-white pb-3">Iniciar Sesión</h1>
+                        <p class="pb-3 mb-3 mb-lg-4 text-white">
+                            No posees cuenta?
+                            <a href="{{ route('register') }}" class="ms-2 text-primary">
+                                Registrate aquí!
+                            </a>
+                        </p>
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="pb-3 mb-3">
+                                <div class="position-relative">
+                                    <i
+                                        class="ri-mail-line fs-lg position-absolute top-50 start-0 translate-middle-y ms-3 text-grey"></i>
+                                    {{-- <input class="form-control form-control-lg ps-5" type="email"
+                                        placeholder="Email address" required /> --}}
+                                    <input id="email" type="email"
+                                        class="form-control form-control-lg ps-5 @error('email') is-invalid @enderror"
+                                        name="email" value="{{ old('email') }}" required autocomplete="email"
+                                        placeholder="Email address" autofocus>
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ 'Estas credenciales no coinciden con nuestros registros.' }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="pb-3 mb-3">
+                                <div class="position-relative">
+                                    <i
+                                        class="ri-lock-line fs-lg position-absolute top-50 start-0 translate-middle-y ms-3 text-grey"></i>
+                                    {{-- <input class="form-control form-control-lg ps-5" type="password" placeholder="Password"
+                                        required /> --}}
+                                    <input id="password" type="password"
+                                        class="form-control form-control-lg ps-5 @error('password') is-invalid @enderror"
+                                        name="password" required placeholder="Password" autocomplete="current-password">
+
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ 'Estas credenciales no coinciden con nuestros registros.' }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            {{-- <button type="submit" class="btn btn-lg btn-primary w-100 mb-4">
+                                Iniciar Sesión
+                            </button> --}}
+                            <button type="submit" class="btn btn-lg btn-primary w-100 mb-4">
+                                {{ __(' Iniciar Sesión') }}
+                            </button>
+
+                            @if (Route::has('password.request'))
+                                <a class="btn text-white" href="{{ route('password.request') }}">
+                                    {{ __('Olvido su contraseña?') }}
+                                </a>
+                            @endif
+                        </form>
+                    </div>
+
+                </div>
+                <div
+                    class="col-lg-6 d-none d-lg-block bg-secondary d-flex align-items-center justify-content-center p-0 imageContainer">
+                    <img src="{{ URL::asset('build/img/finanza_bg.jpg') }}" alt="Login" width="1920" height="1080"
+                        class="img-fluid fullWidthHeight" />
+                </div>
+            </div>
+        </div>
+    </section>
     {{-- <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
@@ -49,7 +161,7 @@
                                 <div class="col-md-6 offset-md-4">
                                     <div class="form-check">
                                         <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                            {{ old('remember') ? 'checked' : '' }}>
+                                            {{ old('remember') ? 'checked' : '' }>
 
                                         <label class="form-check-label" for="remember">
                                             {{ __('Remember Me') }}
@@ -77,132 +189,9 @@
             </div>
         </div>
     </div> --}}
-    <div class="auth-page-wrapper pt-5">
-        <!-- auth page bg -->
-        <div class="auth-one-bg-position auth-one-bg" id="auth-particles">
-            <div class="bg-overlay"></div>
-
-            <div class="shape">
-                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink"
-                    viewBox="0 0 1440 120">
-                    <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
-                </svg>
-            </div>
-        </div>
-
-        <!-- auth page content -->
-        <div class="auth-page-content">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="text-center mt-sm-5 mb-4 text-white-50">
-                            <div>
-                                <a href="index" class="d-inline-block auth-logo">
-                                    <img src="{{ URL::asset('build/img/logo.png') }}" alt="" height="100">
-                                </a>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-                <!-- end row -->
-
-                <div class="row justify-content-center">
-                    <div class="col-md-8 col-lg-6 col-xl-5">
-                        <div class="card mt-4">
-
-                            <div class="card-body p-4">
-                                <div class="text-center mt-2">
-                                    <h5 class="text-primary">Bienvenido !</h5>
-                                    <p class="text-muted">Inicia Sesión para continuar.</p>
-                                </div>
-                                <div class="p-2 mt-4">
-                                    <form method="POST" action="{{ route('login') }}" autocomplete="off">
-                                        @csrf
-
-                                        <div class="mb-3">
-                                            <label for="email" class="form-label">Email</label>
-                                            <input id="email" type="email"
-                                                class="form-control @error('email') is-invalid @enderror" name="email"
-                                                value="{{ old('email') }}" autofocus placeholder="Ingrese Email" required>
-                                            @error('email')
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
-
-                                        <div class="mb-3">
-                                            <div class="float-end">
-                                                @if (Route::has('password.request'))
-                                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                                        {{ __('Olvido su Contraseña?') }}
-                                                    </a>
-                                                @endif
-                                                {{-- <a href="auth-pass-reset-basic" class="text-muted">Olvido su Contraseña?</a> --}}
-                                            </div>
-                                            <label class="form-label" for="password-input">Contraseña</label>
-                                            <div class="position-relative auth-pass-inputgroup mb-3">
-                                                <input id="password" type="password" placeholder="Ingrese Contraseña"
-                                                    class="form-control pe-5 password-input @error('password') is-invalid @enderror"
-                                                    name="password" required>
-                                                <button
-                                                    class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon"
-                                                    type="button" id="password-addon"><i
-                                                        class="ri-eye-fill align-middle"></i></button>
-                                            </div>
-                                        </div>
-
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" value="" id="remember">
-                                            <label class="form-check-label" for="remember">Recordar</label>
-                                        </div>
-
-                                        <div class="mt-4">
-                                            <button class="btn btn-success w-100" type="submit">
-                                                {{ __('Iniciar Sesión') }}</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                            <!-- end card body -->
-                        </div>
-                        <!-- end card -->
-
-                        <div class="mt-4 text-center">
-                            <p class="mb-0">No posee Cuenta? <a href="register"
-                                    class="fw-semibold text-primary text-decoration-underline"> Registrarse </a> </p>
-                        </div>
-
-                    </div>
-                </div>
-                <!-- end row -->
-            </div>
-            <!-- end container -->
-        </div>
-        <!-- end auth page content -->
-
-        <!-- footer -->
-        <footer class="footer start-0">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="text-center">
-                            <script>
-                                document.write(new Date().getFullYear())
-                            </script> Velzon. Crafted with <i class="mdi mdi-heart text-danger"></i> by
-                            Themesbrand</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
-        <!-- end Footer -->
-    </div>
-    <!-- end auth-page-wrapper -->
 @endsection
 @section('script')
-    <script src="{{ URL::asset('build/libs/particles.js/particles.js') }}"></script>
-    <script src="{{ URL::asset('build/js/pages/particles.app.js') }}"></script>
+    {{-- <script src="{{ URL::asset('build/libs/particles.js/particles.js') }}"></script> --}}
+    {{-- <script src="{{ URL::asset('build/js/pages/particles.app.js') }}"></script> --}}
     <script src="{{ URL::asset('build/js/pages/password-addon.init.js') }}"></script>
 @endsection
